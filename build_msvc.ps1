681e621c0d6f1b72ccbb962d0abd9529af5905b6
Connor Hilarides <connorcpu@live.com>
2016-05-20 17:55:18 -0700

    Update build script

--- build_msvc.ps1
+++ build_msvc.ps1
@@ -3,28 +3,29 @@ If (Test-Path "bin") {
     Remove-Item -Recurse -Force "bin"
 }
 
-New-Item "bin" -ItemType Directory
+New-Item "bin" -ItemType Directory > $null
 Push-Location "bin"
-New-Item "x64" -ItemType Directory
-New-Item "x86" -ItemType Directory
+New-Item "x64" -ItemType Directory > $null
+New-Item "x86" -ItemType Directory > $null
 Pop-Location
 
 ###
 # Build serialization library
 
+rustup update stable-x86_64-pc-windows-msvc
+rustup update stable-i686-pc-windows-msvc
+
 # x64
 Push-Location lib\serialization
-rustup update nightly-2016-05-10-x86_64-pc-windows-msvc
-rustup run nightly-2016-05-10-x86_64-pc-windows-msvc cargo clean
-rustup run nightly-2016-05-10-x86_64-pc-windows-msvc cargo build --release
+rustup run stable-x86_64-pc-windows-msvc cargo clean
+rustup run stable-x86_64-pc-windows-msvc cargo build --release
 Pop-Location
 Copy-Item -Path ".\lib\serialization\target\release\serialization.*" -Destination "bin\x64"
 
 # x86
 Push-Location lib\serialization
-rustup update nightly-2016-05-10-i686-pc-windows-msvc
-rustup run nightly-2016-05-10-i686-pc-windows-msvc cargo clean
-rustup run nightly-2016-05-10-i686-pc-windows-msvc cargo build --release
+rustup run stable-i686-pc-windows-msvc cargo clean
+rustup run stable-i686-pc-windows-msvc cargo build --release
 Pop-Location
 Copy-Item -Path ".\lib\serialization\target\release\serialization.*" -Destination "bin\x86"
 
